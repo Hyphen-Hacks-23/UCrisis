dfa18ed11a2ecc3ad72e51ecb0433e437e8ea50f
smukhyala <smukhyala@gmail.com>
2023-09-30 16:59:34 -0700

    cleanup

--- sideTab.py
+++ sideTab.py
@@ -4,13 +4,6 @@ from PIL import Image, ImageTk
 
 def LeftWing_init(tab):
 
-
-    buffer = Label(tab,
-        text="",
-        height = 2,
-        wraplength=200
-    )
-
     TitleName = Label(tab, 
         text="Title:",
         width = 20, 
@@ -18,21 +11,16 @@ def LeftWing_init(tab):
         fg = "white",
         bg = f"#333333",
         wraplength=200
-    )#.pack(side = "top")
+    )
     TitleName.pack(side = "top", padx=10, pady = (10,0), anchor="e")
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     global Title
     Title = Label(tab, 
         text="title",
         width = 20, 
         height = 5,
-    )#.pack(side = "top")
+    )
     Title.pack(side = "top", anchor="e", padx=10)
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     srcURLName = Label(tab, 
         text="Source URL:",
@@ -40,21 +28,16 @@ def LeftWing_init(tab):
         height = 1,
         fg = "white",
         bg = f"#333333",
-    )#.pack(side = "top")
+    )
     srcURLName.pack(side = "top", anchor="e", padx=10)
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     global srcURL
     srcURL = Label(tab, 
         text="url",
         width = 20, 
         height = 5,
-    )#.pack(side = "top")
+    )
     srcURL.pack(side = "top", anchor="e", padx=10)
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     AddyName = Label(tab, 
         text="Address:",
@@ -63,21 +46,16 @@ def LeftWing_init(tab):
         fg = "white",
         bg = f"#333333",
         wraplength=200
-    )#.pack(side = "top")
+    )
     AddyName.pack(side = "top", anchor="e", padx=10)
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     global Addy
     Addy = Label(tab, 
         text="address",
         width = 20, 
         height = 5,
-    )#.pack(side = "top")
+    )
     Addy.pack(side = "top", anchor="e", padx=10)
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     DescName = Label(tab, 
         text="Description:",
@@ -85,21 +63,16 @@ def LeftWing_init(tab):
         height = 1,
         fg = "white",
         bg = f"#333333",
-    )#.pack(side = "top")
+    )
     DescName.pack(side = "top", anchor="e", padx=10)
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     global Desc
     Desc = Label(tab, 
         text="desc",
         width = 20, 
         height = 5,
-    )#.pack(side = "top")
+    )
     Desc.pack(side = "top", anchor="e", padx=10)
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     TimeName = Label(tab, 
         text="Time",
@@ -107,19 +80,16 @@ def LeftWing_init(tab):
         height = 1,
         fg = "white",
         bg = f"#333333",
-    )#.pack(side = "top")
+    )
     TimeName.pack(side = "top", anchor="e", padx=10)
-
-    #buffer.pack(side = "top", anchor="e", padx=10)
 
     global Time
     Time = Label(tab, 
         text="url",
         width = 20, 
         height = 5,
-    )#.pack(side = "top")
+    )
     Time.pack(side = "top", anchor="e", padx=10)
-
     
 def update_info(title, url, desc, address, time):
     Title.configure(text=title)
