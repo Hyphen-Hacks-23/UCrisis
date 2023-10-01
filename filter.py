fa44cb2b968791feb48905fc5a5494d1c0207098
Eric Liu <25liue03@stu.smuhsd.org>
2023-09-30 18:57:30 -0700

    change

--- filter.py
+++ filter.py
@@ -15,17 +15,19 @@ import io
 import os
 import openai
 
-LLM_prompt = "I will give you a list of prompts that revolve around crises/incidents, and I would like you to output a list. Within this list, I would like you to output one line per prompt, prompts separated by a blank line. The lines will include: what the crisis/incident is, the Area/Address/Region of where the crisis/incident happened (no commas between the address, city, and country please), a description of the original crisis message, and the number that was given in the list. Even if a prompt doesn’t specify an address, please use context to try and give an approximate region/area as to where this happened. This list can only have crises that are WITHIN a 40 mile RADIUS of the location that I will give you. If the crisis is not within a 40 mile radius of the given location, DO NOT PRINT IT OUT. If the crisis does not have an address mention in it, DO NOT PRINT IT OUT. For each separate prompt, I will give the text that is in the prompt, a link to where the prompt was posted, and the time it was posted. Each individual prompt outputed in the list must be unique; that is if two prompts describe the same incident/crisis, output only one line for both prompts. The location is: "
+LLM_prompt = "I will give you a list of prompts that revolve around crises/incidents, and I would like you to output a list. Within this list, I would like you to output one line per prompt, prompts separated by a blank line. The lines will include: what the crisis/incident is, the Area/Address/Region of where the crisis/incident happened (no commas between the address, city, and country please), a description of the original crisis message, and the number that was given in the list. Even if a prompt doesn’t specify an address, please use context to try and give an approximate region/area as to where this happened. This list can only have crises that are WITHIN a 40 mile RADIUS of the location that I will give you. If the crisis is not within a 40 mile radius of the given location, DO NOT PRINT IT OUT. If the crisis does not have an address mention in it, DO NOT PRINT IT OUT. For each separate prompt, I will give the text that is in the prompt, a link to where the prompt was posted, and the time it was posted. Each individual prompt outputed in the list must be unique; that is if two prompts describe the same incident/crisis, output only one line for both prompts. Example: Crisis/Incident: Gas Leak (nextline) Area/Address/Region: 550 West Diagonal St George (nextline) Description: Strong smell of gas- All occupants have exited the condo.(nextline) Number: 10   The location is: "
 df = pd.read_csv("facebook_postsold.csv")
 
 links = df[['Number','Post URL','Time']]
 newdf = df[['Number','Text']]
+
+
 prompt_list = newdf.to_csv(index=False)
 
 user_location = input("Please enter your location: ")
 
 # GPT
-api_key = "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"
+api_key = "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"
 openai.api_key = api_key
 
 system_msg = "You are a helpful assistant who knows the map of the world and knows distances between different addresses."
@@ -44,7 +46,7 @@ response = openai.ChatCompletion.create(
 
 generated_text = response["choices"][0]["message"]["content"]
 
-# print(generated_text)
+print(generated_text)
 
 
 crisis_reports = re.split(r"\n\n+", generated_text.strip())
@@ -58,10 +60,10 @@ link_list = []
 # Extract data from each crisis report
 for report in crisis_reports:
     # Use regular expressions to extract data
-    crisis_match = re.search(r"Crisis:\s+(.+)", report)
-    area_match = re.search(r"Area:\s+(.+)", report)
+    crisis_match = re.search(r"Crisis/Incident:\s+(.+)", report)
+    area_match = re.search(r"Area/Address/Region:\s+(.+)", report)
     description_match = re.search(r"Description:\s+(.+)", report)
-    link_match = re.search(r"Link:\s+\[Original Post\]\((.+)\)", report)
+    link_match = re.search(r"Number:\s+(.+)", report)
 
     if crisis_match and area_match and description_match and link_match:
         crisis_list.append(crisis_match.group(1))
@@ -74,9 +76,34 @@ data = {
     "Crisis": crisis_list,
     "Area": area_list,
     "Description": description_list,
-    "Link": link_list,
+    "Number": link_list,
 }
 df = pd.DataFrame(data)
+
+
+# Initialize empty lists to store 'Post URL' and 'Time' values
+post_url_list = []
+time_list = []
+
+# Iterate through the sample DataFrame and populate 'Post URL' and 'Time'
+
+for index, row in df.iterrows():
+    number = str.strip(row['Number'])
+    print(number)
+    # Use the 'Number' value to find the corresponding row in 'df'
+    matching_row = links[links['Number'] == number]
+    if not matching_row.empty:
+        post_url = matching_row.iloc[0]['Post URL']
+        time = matching_row.iloc[0]['Time']
+        post_url_list.append(post_url)
+        time_list.append(time)
+    else:
+        post_url_list.append(None)
+        time_list.append(None)
+
+# Add 'Post URL' and 'Time' columns to the sample DataFrame
+df['Post URL'] = post_url_list
+df['Time'] = time_list
 
 # Save the DataFrame to a CSV file
 csv_file_path = "crisis_data.csv"
